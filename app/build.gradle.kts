57086bbcceda5bffc1a14a85c5c3c5a842565ff3
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-02 12:44:32 +0200

    We add the main Fragment container to the view android:name="androidx.navigation.fragment.NavHostFragment"  -> we need to put this because it will not work without app:dafaultNavHost= "true"   ->  This it will say that is the main fragment container app:navGraph= "@navigation/main_graph"   ->  With this it show what we want

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48")
     kapt("com.google.dagger:hilt-compiler:2.48")
 
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
