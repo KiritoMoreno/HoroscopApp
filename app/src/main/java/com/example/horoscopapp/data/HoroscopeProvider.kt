d35dc8efe2318d171b642623f93ac280b26767ad
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-02 19:09:24 +0200

    we modified the code to created a courutine and add new class HoroscopeProvider to get our List that we going to used in the class HoroscopeViewModel.

---
+++ app/src/main/java/com/example/horoscopapp/data/HoroscopeProvider.kt
@@ -0,0 +1,23 @@
+package com.example.horoscopapp.data
+
+import com.example.horoscopapp.domain.model.HoroscopeInfo
+import javax.inject.Inject
+
+class HoroscopeProvider @Inject constructor() {
+    fun getHoroscopes(): List<HoroscopeInfo> {
+        return listOf(
+            HoroscopeInfo.Aries,
+            HoroscopeInfo.Taurus,
+            HoroscopeInfo.Gemini,
+            HoroscopeInfo.Cancer,
+            HoroscopeInfo.Leo,
+            HoroscopeInfo.Virgo,
+            HoroscopeInfo.Libra,
+            HoroscopeInfo.Scorpio,
+            HoroscopeInfo.Sagittarius,
+            HoroscopeInfo.Capricorn,
+            HoroscopeInfo.Aquarius,
+            HoroscopeInfo.Pisces
+        )
+    }
+}
--- app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/HoroscopeFragment.kt
@@ -1,13 +1,18 @@
 package com.example.horoscopapp.ui.horoscope
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.example.horoscopapp.databinding.FragmentHoroscopeBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 
 @AndroidEntryPoint
@@ -17,6 +22,33 @@ class HoroscopeFragment : Fragment() {
     private val binding get() = _binding!!
     private val horoscopeViewModel by viewModels <HoroscopeViewModel>()
 
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initList()
+        initUIState()
+    }
+
+    private fun initUIState() {
+        // we created a special courutine, is atached with the cicle of life of the fragment
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeViewModel.horoscope.collect {
+                    //Cambios en Horoscope
+                    Log.i("Helloooooo", it.toString())
+                }
+            }
+        }
+    }
+
+    private fun initList() {
+        initUIState()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
