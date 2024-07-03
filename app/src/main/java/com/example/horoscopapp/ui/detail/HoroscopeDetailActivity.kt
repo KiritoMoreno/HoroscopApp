e5bb9ac24d9077df4f9d0083f7c091e38f8d22dd
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-03 16:19:25 +0200

    modify

--- app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
@@ -5,17 +5,18 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import com.example.horoscopapp.R
 import com.example.horoscopapp.databinding.ActivityHoroscopeDetailBinding
+import com.example.horoscopapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityHoroscopeDetailBinding
+    private lateinit var binding : ActivityMainBinding
     private val horoscopeDetailViewModel : HoroscopeDetailViewModel by viewModels()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
