a835d29c050878c072ee0b61e06bfd8500682a19
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-03 16:13:15 +0200

    New Activity and class

---
+++ app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,23 @@
+package com.example.horoscopapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.horoscopapp.R
+import com.example.horoscopapp.databinding.ActivityHoroscopeDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel : HoroscopeDetailViewModel by viewModels()
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
