f4ca1f5a800541714f84cd85fedc183e40e74681
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-03 16:52:29 +0200

    We recover  the item

--- app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,6 +3,7 @@ package com.example.horoscopapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.navigation.navArgs
 import com.example.horoscopapp.R
 import com.example.horoscopapp.databinding.ActivityHoroscopeDetailBinding
 import com.example.horoscopapp.databinding.ActivityMainBinding
@@ -12,13 +13,13 @@ import dagger.hilt.android.AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding : ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel : HoroscopeDetailViewModel by viewModels()
-
+    private val args: HoroscopeDetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        args.type
     }
 }
