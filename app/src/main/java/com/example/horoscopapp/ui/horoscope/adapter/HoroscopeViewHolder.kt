87d1579958ff984d38f0d6754b4193205a830bd4
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-03 15:53:40 +0200

    Rotation animation

--- app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
+++ app/src/main/java/com/example/horoscopapp/ui/horoscope/adapter/HoroscopeViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.horoscopapp.ui.horoscope.adapter
 
 import android.view.View
+import android.view.animation.LinearInterpolator
 import androidx.recyclerview.widget.RecyclerView
 import com.example.horoscopapp.databinding.ItemHoroscopeBinding
 import com.example.horoscopapp.domain.model.HoroscopeInfo
@@ -15,6 +16,17 @@ class HoroscopeViewHolder(view: View) : RecyclerView.ViewHolder(view){
         binding.ivHoroscope.setImageResource(horoscopeInfo.img)
         binding.tvTitle.text = context.getString(horoscopeInfo.name)
 
-        binding.itemHoroscopeParent.setOnClickListener { onItemSelected(horoscopeInfo) }
+        binding.itemHoroscopeParent.setOnClickListener {
+        //onItemSelected(horoscopeInfo)
+        }
+    }
+
+    private fun startRotationAnimation(view: View){
+        view.animate().apply {
+            duration = 500
+            interpolator = LinearInterpolator()
+            rotationBy(360f)
+            start()
+        }
     }
 }
