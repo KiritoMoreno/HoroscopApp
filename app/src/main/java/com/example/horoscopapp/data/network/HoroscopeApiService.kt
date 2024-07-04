e46385f24c93ea310e41ecb4cd197de54b9262b8
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-04 14:03:12 +0200

    Retrofit it will recover the value of the json to the variable.

--- app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
+++ app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
@@ -1,10 +1,11 @@
 package com.example.horoscopapp.data.network
+import com.example.horoscopapp.data.network.response.PredictionResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface HoroscopeApiService {
 
     @GET("/{sing}")
-    suspend fun getHoroscope(@Path("sign") sign: String)
-    
+    suspend fun getHoroscope(@Path("sign") sign: String): PredictionResponse
+
 }
