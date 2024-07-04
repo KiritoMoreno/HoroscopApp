f70b0903030616217ce0e8953c1cd2fd2676b204
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-04 13:39:43 +0200

    We created a coroutine because we needed to create a second thread. The name of the path tag has to be the same as in the GET request.

--- app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
+++ app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
@@ -1,4 +1,10 @@
 package com.example.horoscopapp.data.network
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface HoroscopeApiService {
+
+    @GET("/{sing}")
+    suspend fun getHoroscope(@Path("sign") sign: String)
+    
 }
