6803f62a969082c9213032b9dbf292138d82346e
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-04 20:08:39 +0200

    We created the Repository interface, which will serve as the communication link between the data and domain.

---
+++ app/src/main/java/com/example/horoscopapp/data/RepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.example.horoscopapp.data
+
+import com.example.horoscopapp.domain.model.Repository
+
+class RepositoryImpl: Repository {
+}
---
+++ app/src/main/java/com/example/horoscopapp/domain/model/Repository.kt
@@ -0,0 +1,4 @@
+package com.example.horoscopapp.domain.model
+
+interface Repository {
+}
