a62cad55459ae9ec406ef8a74074c1f10ca81b17
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-04 12:33:42 +0200

    Change the mistakes in the layout f activity_horoscope_detail and add the logic of horoscopeDetailActivity to be linked to the lifecycle of horoscopeViewModel. This way, we will collect it properly, our var 'state' will only give us one of the horoscopeDetailState.

--- app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,11 +3,17 @@ package com.example.horoscopapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.example.horoscopapp.R
 import com.example.horoscopapp.databinding.ActivityHoroscopeDetailBinding
 import com.example.horoscopapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
@@ -19,8 +25,35 @@ class HoroscopeDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        args.type
+        initUI()
+        //args.type
         
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED){
+                horoscopeDetailViewModel.state.collect{
+                    when(it){
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+    private fun loadingState(){
+        binding.progressBar.isVisible = true    
+    }
+    private fun errorState(){
+
+    }
+    private fun successState(){
+
     }
 }
