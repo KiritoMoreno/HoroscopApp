284eda124562861ae4ad03ed7903c04c238a4d2b
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-09 11:42:55 +0200

    With the operator, we can override some functions in the creation of this class. Using invoke allows me to skip adding another method and call it directly.

--- app/src/main/java/com/example/horoscopapp/domain/model/usecase/GetPredictionUseCase.kt
+++ app/src/main/java/com/example/horoscopapp/domain/model/usecase/GetPredictionUseCase.kt
@@ -3,7 +3,7 @@ package com.example.horoscopapp.domain.model.usecase
 import com.example.horoscopapp.domain.model.Repository
 import javax.inject.Inject
 
-class GetPredictionUseCase @Inject constructor(private val respository: Repository){
+class GetPredictionUseCase @Inject constructor(private val repository: Repository){
 
-
+    suspend operator fun invoke(sign: String) = repository.getPrediction(sign)
 }
--- app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/example/horoscopapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.horoscopapp.ui.detail
 
 import androidx.lifecycle.ViewModel
+import com.example.horoscopapp.domain.model.usecase.GetPredictionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor(private val getPredictionUseCase: GetPredictionUseCase): ViewModel() {
 
     private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading) // Start State
     val state : StateFlow<HoroscopeDetailState> = _state
+
+
+    fun example(){
+        getPredictionUseCase() // we call the function from GetPredictionUseCase.kt
+    }
 }
