3b552152ff6fe6c4ef533277aaba95eccef797b1
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-10 13:09:49 +0200

    Correting typo

--- app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
+++ app/src/main/java/com/example/horoscopapp/data/network/HoroscopeApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.Path
 
 interface HoroscopeApiService {
 
-    @GET("/{sing}")
+    @GET("/{sign}")
     suspend fun getHoroscope(@Path("sign") sign: String): PredictionResponse
 
 }
