90cb7bb2137cb54e254e32fdde508ee02faf42c5
Kirito Moreno <99137444+KiritoMoreno@users.noreply.github.com>
2024-07-16 15:59:14 +0200

    in the MainActivityTest we created anothe Test. When it click on a real item in the recyclerView, verify that it goes to the datail view.

--- app/src/androidTest/java/com/example/horoscopapp/ui/home/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/horoscopapp/ui/home/MainActivityTest.kt
@@ -1,13 +1,20 @@
 package com.example.horoscopapp.ui.home
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.horoscopapp.R
+import com.example.horoscopapp.ui.detail.HoroscopeDetailActivity
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.After
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
@@ -30,12 +37,27 @@ class MainActivityTest{
     fun setUp(){
         hiltRule.inject()
 
-
+        // Prepare the intents
+        Intents.init()
     }
 
+    @After
+    fun tearDown(){
+        Intents.release()
+    }
     @Test
     fun `when_main_activity_is_created_then_open_LuckFragment`(){
         onView(withId(R.id.luckFragment)).perform(click())
 
+    }
+
+    @Test
+    fun `when_horoscope_is_selected_then_open_detail`(){
+        onView(withId(R.id.rvHoroscope)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0, click()
+            )
+        )
+        intended(hasComponent(HoroscopeDetailActivity::class.java.name))
     }
 }
